c2ef433bd23ddf0ebc66ede746357532fe3e7671
Zeljko <zel87king2@gmail.com>
2025-05-21 07:39:57 +0200

    update 16

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,59 +1,43 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: "7%",
-        right: "1%",
-        // transform: "translate(-50%, -50%)",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "10px",
-        zIndex: 5, // Osigurava da je iznad pozadine
-        color: "#4d3d63",
-        // textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-      }}
-    >
+    <div className="contact-container">
       {/* Vertikalni tekst KONTAKT */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontSize: "3.7rem",
-          // fontWeight: "bold",
-          marginBottom: "6rem",
-          lineHeight: "1",
-        }}
-      >
+      <div className="contact-text">
         {"KONTAKT".split("").map((letter, index) => (
           <span key={index}>{letter}</span>
         ))}
       </div>
 
-      {/* Prva slika */}
-      <img
-        src="/facebook.png"
-        alt="Slika 1"
-        style={{
-          width: "93px",
-          height: "122px",
-          marginBottom: "1rem",
-        }}
-      />
+      {/* Facebook link */}
+      <a
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-link"
+      >
+        <img
+          src="/facebook.png"
+          alt="Facebook"
+          className="contact-image first-image"
+        />
+      </a>
 
-      {/* Druga slika */}
-      <img
-        src="/instagram.png"
-        alt="Slika 2"
-        style={{
-          width: "100px",
-          height: "auto",
-        }}
-      />
+      {/* Instagram link */}
+      <a
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-link"
+      >
+        <img
+          src="/instagram.png"
+          alt="Instagram"
+          className="contact-image second-image"
+        />
+      </a>
     </div>
   );
 };
